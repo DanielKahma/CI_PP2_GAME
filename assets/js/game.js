e5956ae0ac59fef7608bcc4d14830ff45fb34061
DanielKahma <Daniel.kahma@hotmail.com>
2023-10-17 23:56:29 +0200

    add winning conditions

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,6 +32,18 @@ function handleCellPlayed(clickedCell, clickedCellIndex){
     clickedCell.innerHTML = currentPlayer;
 }
 
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    
+];
+
 
 
 
