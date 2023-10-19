883b356621b2831ab09bebd1edcd3395f7acf62f
DanielKahma <Daniel.kahma@hotmail.com>
2023-10-19 20:08:55 +0200

    add js for questions, as well as answers

---
+++ assets/js/quiz.js
@@ -0,0 +1,48 @@
+const questions = [
+    {
+        question: "How many months do I think there should be in 1 year?",
+        answers: [
+            {text: "11", correct: false},
+            {text: "12", correct: false},
+            {text: "13", correct: true},
+            {text: "14", correct: false}
+        ]
+    },
+    {
+    question: "How many islands does Sweden have?",
+        answers: [
+            {text: "162,486", correct: false},
+            {text: "267,570", correct: true},
+            {text: "18", correct: false},
+            {text: "211,380", correct: false}
+        ]
+    },
+    {
+        question: "How many legs does Odin's horse 'Sleipnir' have?",
+        answers: [
+            {text: "2", correct: false},
+            {text: "4", correct: false},
+            {text: "6", correct: false},
+            {text: "8", correct: true}
+        ]
+    },
+    {
+        question: "Who is the author of the popular Japanese manga One Piece?",
+        answers: [
+            {text: "Eiichiro Oda", correct: true},
+            {text: "Akira Toriyama", correct: false},
+            {text: "Masashi Kishimoto", correct: false},
+            {text: "Noriaki Kubo", correct: false}
+        ]
+    },
+    {
+        question: "What is the largest animal in the world?",
+        answers: [
+            {text: "Elephant", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "giraffe", correct: false},
+            {text: "A large dog", correct: false}
+        ]
+    }
+
+];
